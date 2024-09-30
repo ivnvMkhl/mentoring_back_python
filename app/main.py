c699b4241b7cd261f0b537df84fea40970e402b9
root <root@cv4151875.novalocal>
2024-09-30 10:37:40 +0300

    add main router prefix

--- app/main.py
+++ app/main.py
@@ -19,45 +19,42 @@ from app.user.router import router as user_router
 from app.user.base_config import auth_backend, fastapi_users
 from sqlalchemy import update
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.config import settings
+from operator import attrgetter
 
+MAIN_API_PREFIX = attrgetter('MAIN_API_PREFIX')(settings)
 
 app = FastAPI(
-    title="mentors_web_app"
+    title="mentors_web_app",
+    docs_url=f"{MAIN_API_PREFIX}/docs",
+    openapi_url=f"{MAIN_API_PREFIX}/openapi.json"
 )
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth/jwt",
+    prefix=f"{MAIN_API_PREFIX}/auth/jwt",
     tags=["auth"],
 )
-
-
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
+    prefix=f"{MAIN_API_PREFIX}/auth",
     tags=["auth"],
 )
-
-
+app.include_router(lesson_router, prefix=MAIN_API_PREFIX)
+app.include_router(menti_router, prefix=MAIN_API_PREFIX)
+app.include_router(user_router, prefix=MAIN_API_PREFIX)
 
 
 current_user = fastapi_users.current_user()
 current_active_user = fastapi_users.current_user(active=True)
 
-@app.get("/protected-route")
+@app.get(f"{MAIN_API_PREFIX}/protected-route")
 def protected_route(user: User = Depends(current_active_user)):
     return f"Hello, {user.user_name}"
 
-
-
-@app.get("/upprotected-route")
+@app.get(f"{MAIN_API_PREFIX}/upprotected-route")
 def unprotected_route(user: User = Depends(current_user)):
     return f"Hello, anonymus"
-
-app.include_router(lesson_router)
-app.include_router(menti_router)
-app.include_router(user_router)
-
 
 #Запуск сервера uvicorn
 if __name__ == "__main__":
